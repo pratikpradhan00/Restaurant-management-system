bdaba0a79caf62c32f2e018a36bc372d0840e5d2
BAKAAYUSH <129573319+BAKAAYUSH@users.noreply.github.com>
2023-05-07 07:58:33 +0545

    Add files via upload

---
+++ RestroGirls/chunks/about-us.php
@@ -0,0 +1,7 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3">this is a project done by Ayush Paradhanang ,Rana dorje ,Renisha Manandhar ,Riyan Pradhan and Pratik Pradhan. this is the projejct for collaborative devolopment. our project manaer is Pratik Pradhan, bussiness analyts is Rana dorje and the developers are Ayush , Renisha and Riyan</p>
+		      </div>
+	</section>
---
+++ RestroGirls/chunks/description.php
@@ -0,0 +1,13 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Resturant Powered By L5CG2</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+		        	<p>hello everyone this ptoject allows you to place order for verity of food. in th admin login you can check the order placed make changes in the menu.</p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/17964.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
